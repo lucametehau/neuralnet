a6b83b3e20b93ed9ff1e5a630c0a0f11e9b84fba
lucametehau <lucametehau@gmail.com>
2021-08-14 13:16:18 +0300

    move NN implementation to separate header, add more activation functions

--- main.cpp
+++ main.cpp
@@ -6,200 +6,10 @@
 #include <ctime>
 #include <cassert>
 #include <queue>
+#include "neuralnet.h"
 #pragma GCC optimize("Ofast")
 
 using namespace std;
-
-namespace tools {
-  mt19937_64 gen(time(0));
-  uniform_real_distribution <double> rng(0, 1);
-  uniform_int_distribution <int> bin(0, 1);
-
-  vector <double> createRandomArray(int length) {
-    vector <double> v;
-
-    for(int i = 0; i < length; i++)
-      v.push_back(rng(gen));
-
-    return v;
-  }
-}
-
-class Layer {
-public:
-  Layer(int numNeurons, int prevNumNeurons) {
-    size = numNeurons;
-    bias = tools::createRandomArray(numNeurons);
-    output.resize(numNeurons);
-    outputDerivative.resize(numNeurons);
-    error.resize(numNeurons);
-    weights.resize(numNeurons);
-
-    if(prevNumNeurons) {
-      for(int i = 0; i < numNeurons; i++) {
-        weights[i].resize(prevNumNeurons);
-        weights[i] = tools::createRandomArray(prevNumNeurons);
-      }
-    }
-  }
-
-  int size;
-  vector <double> bias, output, error, outputDerivative;
-  vector <vector <double>> weights;
-};
-
-class Network {
-public:
-
-  Network(vector <int> &topology) {
-    for(int i = 0; i < (int)topology.size(); i++) {
-      layers.push_back(Layer(topology[i], (i > 0 ? topology[i - 1] : 0)));
-    }
-  }
-
-  double activationFunction(double x) {
-    return 1.0 / (1.0 + exp(-x));
-  }
-
-  double activationFunctionDerivative(double x) {
-    return activationFunction(x) * (1 - activationFunction(x));
-  }
-
-  vector <double> calc(vector <double> &input) { /// feed forward
-    //cout << layers.size() << " " << layers[0].size << " " << input.size() << "\n";
-    layers[0].output = input;
-
-    //cout << "a\n";
-
-    for(int l = 1; l < (int)layers.size(); l++) {
-      for(int n = 0; n < layers[l].size; n++) {
-        double sum = layers[l].bias[n];
-
-        for(int prevN = 0; prevN < layers[l - 1].size; prevN++) {
-          sum += layers[l - 1].output[prevN] * layers[l].weights[n][prevN];
-        }
-
-        layers[l].output[n] = activationFunction(sum);
-        layers[l].outputDerivative[n] = activationFunctionDerivative(sum);
-      }
-    }
-
-    //cout << "a\n";
-
-    return layers.back().output;
-  }
-
-  void backProp(vector <double> &target) {
-    /// for output neurons
-    for(int n = 0; n < layers.back().size; n++) {
-      layers.back().error[n] = (layers.back().output[n] - target[n]) * layers.back().outputDerivative[n];
-    }
-
-    /// for hidden layers
-    for(int l = (int)layers.size() - 2; l > 0; l--) {
-      for(int n = 0; n < layers[l].size; n++) {
-        double sum = 0;
-        for(int nextN = 0; nextN < layers[l + 1].size; nextN++)
-          sum += layers[l + 1].weights[nextN][n] * layers[l + 1].error[nextN];
-
-        layers[l].error[n] = sum * layers[l].outputDerivative[n];
-      }
-    }
-  }
-
-  void updateWeights(double LR) {
-    for(int l = 1; l < (int)layers.size(); l++) {
-      for(int n = 0; n < layers[l].size; n++) {
-        for(int prevN = 0; prevN < layers[l - 1].size; prevN++) {
-          double delta = -LR * layers[l - 1].output[prevN] * layers[l].error[n];
-          layers[l].weights[n][prevN] += delta;
-        }
-
-        /// update bias
-        /// no more layers[l - 1].output[prevN], because bias isn't connected to previous neurons
-        double delta = -LR * layers[l].error[n];
-
-        layers[l].bias[n] += delta;
-      }
-    }
-  }
-
-  void train(vector <double> &input, vector <double> &target, double LR) {
-    if((int)input.size() != layers[0].size) {
-      cout << "Wrong input size!\n";
-      assert(0);
-    }
-
-    if((int)target.size() != layers.back().size) {
-      cout << "Wrong output size!\n";
-      assert(0);
-    }
-
-    calc(input);
-    backProp(target);
-    updateWeights(LR);
-  }
-
-  void write(vector <double> &v, ofstream &out) {
-    for(auto &i : v)
-      out << i << " ";
-    out << "\n";
-  }
-
-  vector <double> read(int lg, ifstream &in) {
-    vector <double> v;
-    double x;
-    for(int i = 0; i < lg; i++)
-      in >> x, v.push_back(x);
-    return v;
-  }
-
-  void save(string path) {
-    ofstream out (path);
-    int cnt = layers.size();
-
-    vector <double> v;
-
-    out << cnt << "\n";
-
-    for(int i = 0; i < (int)layers.size(); i++) {
-      write(layers[i].bias, out);
-      write(layers[i].output, out);
-      write(layers[i].outputDerivative, out);
-      write(layers[i].error, out);
-
-      for(int j = 0; j < layers[i].size && i; j++) {
-        write(layers[i].weights[j], out);
-      }
-    }
-  }
-
-  void load(string path) {
-    ifstream in (path);
-    int cnt = layers.size(), cnt2 = 0;
-
-    in >> cnt2;
-
-    if(cnt2 != cnt) {
-      cout << "Can't load network!\n";
-      cout << "Expected " << cnt << ", got " << cnt2 << "\n";
-      assert(0);
-    }
-
-    for(int i = 0; i < (int)layers.size(); i++) {
-      layers[i].bias = read(layers[i].size, in);
-      layers[i].output = read(layers[i].size, in);
-      layers[i].outputDerivative = read(layers[i].size, in);
-      layers[i].error = read(layers[i].size, in);
-
-      for(int j = 0; j < layers[i].size && i; j++) {
-        layers[i].weights[j] = read(layers[i - 1].size, in);
-      }
-    }
-  }
-
-  vector <Layer> layers;
-};
 
 namespace training {
   /// change these values to adjust training data size
@@ -318,7 +128,9 @@ void test() {
   bool create = false; /// change this to true if you want a fresh new dataset
   int dataSize = 1000000;
   int epochs = 100000;
-  double LR = 0.1;
+  double LR = 0.01;
+  double split = 0.1;
+  int trainSize = dataSize * (1.0 - split);
 
   if(create) {
     training::createDataset(dataSize, "training.txt");
@@ -329,43 +141,25 @@ void test() {
 
   topology.push_back(36);
   topology.push_back(12);
-  topology.push_back(6);
+  //topology.push_back(6);
   topology.push_back(1);
 
   Network NN(topology);
 
-  NN.load("mazesolver.nn");
+  //NN.load("mazesolver_relu.nn");
 
-  double split = 0.1;
-  int trainSize = dataSize * split;
   vector <vector <double>> input, output;
 
   training::readDataset(input, output, dataSize, "training.txt");
 
-  for(int epoch = 1; epoch <= epochs; epoch++) {
-    cout << "----------------------------------------- Epoch " << epoch << "/" << epochs << " -----------------------------------------\n";
-    for(int i = 0; i < trainSize; i++) {
-      NN.train(input[i], output[i], LR);
-    }
+  /*int acc = 0;
 
-    double error = 0;
-
-    for(int i = trainSize; i < dataSize; i++) {
-      vector <double> ans = NN.calc(input[i]);
-
-      double delta = (ans[0] - output[i][0]);
-      error += delta * delta;
-    }
-
-    cout << "Total error: " << error / (2 * (dataSize - trainSize)) << "\n";
-
-    NN.save("mazesolver.nn");
-  }
-
-  /*for(int i = trainSize; i < dataSize; i++) {
+  for(int i = trainSize; i < dataSize; i++) {
     vector <double> ans = NN.calc(input[i]);
 
-    if(i % 10 == 0) {
+    acc += (round(ans[0]) == output[i][0]);
+
+    if(i % 100 == 0) {
       cout << "Index : " << i << "\n";
 
       cout << "Input : ";
@@ -383,7 +177,40 @@ void test() {
         cout << j << " ";
       cout << "\n";
     }
-  }*/
+  }
+
+  cout << "Accuracy: " << 100.0 * acc / (dataSize - trainSize) << "%\n";
+
+  return;*/
+
+  for(int epoch = 1; epoch <= epochs; epoch++) {
+    cout << "----------------------------------------- Epoch " << epoch << "/" << epochs << " -----------------------------------------\n";
+
+    double tStart = clock();
+
+    for(int i = 0; i < trainSize; i++) {
+      NN.train(input[i], output[i], LR);
+    }
+
+    double error = 0;
+
+    for(int i = trainSize; i < dataSize; i++) {
+      vector <double> ans = NN.calc(input[i]);
+
+      double delta = (ans[0] - output[i][0]);
+      error += delta * delta;
+    }
+
+    double tEnd = clock();
+
+    cout << "Raw error : " << error << "\n";
+
+    cout << "Error     : " << error / (2 * (dataSize - trainSize)) << "\n";
+
+    cout << "Time taken: " << (tEnd - tStart) / CLOCKS_PER_SEC << " s\n";
+
+    NN.save("mazesolver_relu.nn");
+  }
 }
 
 int main() {
---
+++ neuralnet.h
@@ -0,0 +1,216 @@
+#pragma once
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+const int SIGMOID    = 1;
+const int RELU       = 2;
+const int LEAKY_RELU = 3;
+const int ACTIVATION = LEAKY_RELU;
+
+namespace tools {
+  mt19937_64 gen(time(0));
+  uniform_real_distribution <double> rng(0, 1);
+  uniform_int_distribution <int> bin(0, 1);
+
+  vector <double> createRandomArray(int length) {
+    vector <double> v;
+
+    for(int i = 0; i < length; i++)
+      v.push_back(rng(gen));
+
+    return v;
+  }
+}
+
+class Layer {
+public:
+  Layer(int numNeurons, int prevNumNeurons) {
+    size = numNeurons;
+    bias = tools::createRandomArray(numNeurons);
+    output.resize(numNeurons);
+    outputDerivative.resize(numNeurons);
+    error.resize(numNeurons);
+    weights.resize(numNeurons);
+
+    if(prevNumNeurons) {
+      for(int i = 0; i < numNeurons; i++) {
+        weights[i].resize(prevNumNeurons);
+        weights[i] = tools::createRandomArray(prevNumNeurons);
+      }
+    }
+  }
+
+  int size;
+  vector <double> bias, output, error, outputDerivative;
+  vector <vector <double>> weights;
+};
+
+class Network {
+public:
+
+  Network(vector <int> &topology) {
+    for(int i = 0; i < (int)topology.size(); i++) {
+      layers.push_back(Layer(topology[i], (i > 0 ? topology[i - 1] : 0)));
+    }
+  }
+
+  double activationFunction(double x) {
+    if(ACTIVATION == SIGMOID)
+      return 1.0 / (1.0 + exp(-x));
+
+    if(x > 0)
+      return x;
+
+    return (ACTIVATION == RELU ? 0 : 0.05 * x);
+  }
+
+  double activationFunctionDerivative(double x) {
+    if(ACTIVATION == SIGMOID)
+      return activationFunction(x) * (1 - activationFunction(x));
+
+    if(x > 0)
+      return 1;
+
+    return (ACTIVATION == RELU ? 0 : 0.05);
+  }
+
+  vector <double> calc(vector <double> &input) { /// feed forward
+    //cout << layers.size() << " " << layers[0].size << " " << input.size() << "\n";
+    layers[0].output = input;
+
+    //cout << "a\n";
+
+    for(int l = 1; l < (int)layers.size(); l++) {
+      for(int n = 0; n < layers[l].size; n++) {
+        double sum = layers[l].bias[n];
+
+        for(int prevN = 0; prevN < layers[l - 1].size; prevN++) {
+          sum += layers[l - 1].output[prevN] * layers[l].weights[n][prevN];
+        }
+
+        layers[l].output[n] = activationFunction(sum);
+        layers[l].outputDerivative[n] = activationFunctionDerivative(sum);
+      }
+    }
+
+    //cout << "a\n";
+
+    return layers.back().output;
+  }
+
+  void backProp(vector <double> &target) {
+    /// for output neurons
+    for(int n = 0; n < layers.back().size; n++) {
+      layers.back().error[n] = (layers.back().output[n] - target[n]) * layers.back().outputDerivative[n];
+    }
+
+    /// for hidden layers
+    for(int l = (int)layers.size() - 2; l > 0; l--) {
+      for(int n = 0; n < layers[l].size; n++) {
+        double sum = 0;
+        for(int nextN = 0; nextN < layers[l + 1].size; nextN++)
+          sum += layers[l + 1].weights[nextN][n] * layers[l + 1].error[nextN];
+
+        layers[l].error[n] = sum * layers[l].outputDerivative[n];
+      }
+    }
+  }
+
+  void updateWeights(double LR) {
+    for(int l = 1; l < (int)layers.size(); l++) {
+      for(int n = 0; n < layers[l].size; n++) {
+        for(int prevN = 0; prevN < layers[l - 1].size; prevN++) {
+          double delta = -LR * layers[l - 1].output[prevN] * layers[l].error[n];
+          layers[l].weights[n][prevN] += delta;
+        }
+
+        /// update bias
+        /// no more layers[l - 1].output[prevN], because bias isn't connected to previous neurons
+        double delta = -LR * layers[l].error[n];
+
+        layers[l].bias[n] += delta;
+      }
+    }
+  }
+
+  void train(vector <double> &input, vector <double> &target, double LR) {
+    if((int)input.size() != layers[0].size) {
+      cout << "Wrong input size!\n";
+      assert(0);
+    }
+
+    if((int)target.size() != layers.back().size) {
+      cout << "Wrong output size!\n";
+      assert(0);
+    }
+
+    calc(input);
+    backProp(target);
+    updateWeights(LR);
+  }
+
+  void write(vector <double> &v, ofstream &out) {
+    for(auto &i : v)
+      out << i << " ";
+    out << "\n";
+  }
+
+  vector <double> read(int lg, ifstream &in) {
+    vector <double> v;
+    double x;
+    for(int i = 0; i < lg; i++)
+      in >> x, v.push_back(x);
+    return v;
+  }
+
+  void save(string path) {
+    ofstream out (path);
+    int cnt = layers.size();
+
+    vector <double> v;
+
+    out << cnt << "\n";
+
+    for(int i = 0; i < (int)layers.size(); i++) {
+      write(layers[i].bias, out);
+      write(layers[i].output, out);
+      write(layers[i].outputDerivative, out);
+      write(layers[i].error, out);
+
+      for(int j = 0; j < layers[i].size && i; j++) {
+        write(layers[i].weights[j], out);
+      }
+    }
+  }
+
+  void load(string path) {
+    ifstream in (path);
+    int cnt = layers.size(), cnt2 = 0;
+
+    in >> cnt2;
+
+    if(cnt2 != cnt) {
+      cout << "Can't load network!\n";
+      cout << "Expected " << cnt << ", got " << cnt2 << "\n";
+      assert(0);
+    }
+
+    for(int i = 0; i < (int)layers.size(); i++) {
+      layers[i].bias = read(layers[i].size, in);
+      layers[i].output = read(layers[i].size, in);
+      layers[i].outputDerivative = read(layers[i].size, in);
+      layers[i].error = read(layers[i].size, in);
+
+      for(int j = 0; j < layers[i].size && i; j++) {
+        layers[i].weights[j] = read(layers[i - 1].size, in);
+      }
+    }
+  }
+
+  vector <Layer> layers;
+};
